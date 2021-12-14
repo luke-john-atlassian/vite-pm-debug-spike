7169a89e083b4559d7f1f5a01e6f2bd1617ccc45
Luke Woollard <lwoollard@atlassain.com>
2021-12-14 09:05:02 +0800

    add basic editor plugins

--- src/Editor.tsx
+++ src/Editor.tsx
@@ -1,13 +1,23 @@
 import { schema } from "prosemirror-schema-basic";
 import { EditorState } from "prosemirror-state";
 import { EditorView } from "prosemirror-view";
+import { baseKeymap } from "prosemirror-commands";
+import { keymap } from "prosemirror-keymap";
+import { undo, redo, history } from "prosemirror-history";
 
 export function Editor() {
   return (
     <div
       style={{ border: "1px solid pink" }}
       ref={(node) => {
-        let state = EditorState.create({ schema });
+        let state = EditorState.create({
+          schema,
+          plugins: [
+            history(),
+            keymap({ "Mod-z": undo, "Mod-y": redo }),
+            keymap(baseKeymap),
+          ],
+        });
         new EditorView(node!, { state });
       }}
     ></div>
