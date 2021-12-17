94dea0e3620183224c5105e4192300ecce9e78cd
Luke Woollard <lwoollard@atlassain.com>
2021-12-17 11:03:21 +0800

    fix transaction view

--- src/debug-ui/LogExplorer/LogEntry/TransactionDetails/TransactionDetails.tsx
+++ src/debug-ui/LogExplorer/LogEntry/TransactionDetails/TransactionDetails.tsx
@@ -73,7 +73,7 @@ export function TransactionDetails({
                         {appendTransaction.duration}
                       </TableDataCell>
                       <TableDataCell>
-                        {appendTransaction.addedTransaction}
+                        {appendTransaction.addedTransaction ? "true" : "false"}
                       </TableDataCell>
                     </TableRow>
                   );
